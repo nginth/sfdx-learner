798c0fa80bdcfd6b1222605a4d53e83e550567e6
Nicholas Ginther <GintherN@mccombs.utexas.edu>
2017-10-24 11:43:49 -0500

    add markers to the map

--- force-app/main/default/aura/AccountList/AccountListController.js
+++ force-app/main/default/aura/AccountList/AccountListController.js
@@ -3,7 +3,10 @@
         var action = component.get("c.findAll");
         action.setCallback(this, function(a) {
             component.set("v.accounts", a.getReturnValue());
+            var event = $A.get("e.c:AccountsLoaded");
+            event.setParams({"accounts": a.getReturnValue()});
+            event.fire();
         });
-        $A.enqueueAction(action);
+    $A.enqueueAction(action);
     }
 })
